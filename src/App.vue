044f97aa134f6007c5299edb9c118ca56d8c19d5
Baldnes <128710377+Baldnes@users.noreply.github.com>
2023-08-01 23:25:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">123
+    <h1></h1>
+  </div>
+</template>
+<script lang="ts">
+var fun =
+    ()
+        =>
+{
+
+}
+</script>
+<style scoped lang="scss">
+.box {
+  width: 200px;
+  height: 220px;
+  background: #888888;
+  h1{width: 100px;}
+}
+</style>
