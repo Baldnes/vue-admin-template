cabf99ed60756c7c9161f2a919adaac3c9eb3188
Baldnes <128710377+Baldnes@users.noreply.github.com>
2023-08-03 22:10:26 +0800

    chore: "二次封装axios"

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="box">
-    <el-button type="primary">123</el-button>
-    <!--    <el-button type="success" :icon="Check" circle />-->
-    <div class="demo-pagination-block">
-      <div class="demonstration">Total item count</div>
-      <el-pagination :page-size="100" :total="1000" />
-    </div>
-  </div>
+  <div>测试axios封装</div>
 </template>
 <script lang="ts" setup>
-// import { Check } from '@element-plus/icons-vue'
+import request from '@/utils/request.ts'
+import {onMounted} from "vue";
+
+onMounted(() => {
+  request({
+    url: "/user/login",
+    method: "post",
+    data: {
+      username: "admin",
+      password: "111111"
+    }
+  }).then(res => {
+    console.log(res)
+  })
+})
 </script>
 <style scoped lang="scss">
-.box {
-  width: 200px;
-  height: 210px;
-  h1 {
-    width: 100px;
-  }
-}
+
 </style>
