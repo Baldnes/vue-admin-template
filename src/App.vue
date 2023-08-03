5bb49334f82b851fab6e95078326f5897a5d1899
Baldnes <128710377+Baldnes@users.noreply.github.com>
2023-08-03 22:11:07 +0800

    chore: "二次封装axios"

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,19 @@
 </template>
 <script lang="ts" setup>
 import request from '@/utils/request.ts'
-import {onMounted} from "vue";
+import { onMounted } from 'vue'
 
 onMounted(() => {
   request({
-    url: "/user/login",
-    method: "post",
+    url: '/user/login',
+    method: 'post',
     data: {
-      username: "admin",
-      password: "111111"
-    }
-  }).then(res => {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((res) => {
     console.log(res)
   })
 })
 </script>
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
